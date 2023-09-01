884c849b38b65354ee2fcacf2bcc6477444f04de
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-09-01 18:51:37 +0300

    Add sqlitecpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <SQLiteCpp/Database.h>
+#include <exception>
 #include <fmt/core.h>
 
-int main()
+int main() try
 {
+	SQLite::Database db {"db.db"};
 	fmt::print("sas.\n");
 	return 0;
+}
+catch(const std::exception & exception)
+{
+	fmt::print("exception: '{}'.\n", exception.what());
 }
